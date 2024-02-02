5bb2c158e4cd4ab999080af261eeb0310fd0e840
DT-L21 <127994542+DT-L21@users.noreply.github.com>
2024-02-02 20:04:12 +0100

    Commit roll back

--- Shared/Views/Live/Root/In-Game/AgentSelectView.swift
+++ Shared/Views/Live/Root/In-Game/AgentSelectView.swift
@@ -163,7 +163,7 @@ struct AgentSelectView: View {
 				icon
 				
 				VStack(alignment: .leading, spacing: 4) {
-					if let playerUser {
+					if !shouldAnonymize (player. id), let playerUser {
 						HStack {
 							Text(playerUser.gameName)
 							Text("#\(playerUser.tagLine)")
